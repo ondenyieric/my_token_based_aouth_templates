5ffe1e1b1ef3c58523c6db59fac684e209216880
ondenyieric <eric.ondenyi@meltwater>
2017-04-11 15:42:09 +0000

    Serialize B**

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'sqlite3'
 gem 'puma', '~> 3.7'
 
 gem 'jwt'
-
+gem 'active_model_serializers', '~> 0.10.0'
 
 gem 'bcrypt', '~> 3.1.7'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+   # module the controllers without affecting the URI
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
+  end
+  
     # namespace the controllers without affecting the URI
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
    resources :todos do
